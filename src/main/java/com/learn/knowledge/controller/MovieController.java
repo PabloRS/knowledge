71cfa4a0f7ab80638d8739817ec3a3e161b42284
PabloRS <prmzs89@hotmail.com>
2022-06-07 21:54:12 -0500

    Update example how to use spring cloud config

--- src/main/java/com/learn/knowledge/controller/MovieController.java
+++ src/main/java/com/learn/knowledge/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.learn.knowledge.controller;
 import com.learn.knowledge.model.Movie;
 import com.learn.knowledge.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
@@ -12,9 +13,14 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    @Value("${app.message}")
+    private String message;
+
     @GetMapping("/{id}")
     public Movie getMovieById(@PathVariable("id") Integer id) {
+        System.out.println(message);
         return movieService.getMovieById(id);
+
     }
 
     @PostMapping("/save")
